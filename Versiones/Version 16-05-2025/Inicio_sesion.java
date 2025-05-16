7ed4681dc8677a25b0dd2c1cf59f78e6c32fb20a
Ruben <rubenroigsoucase@gmail.com>
2025-05-16 11:48:29 +0200

    funciona login y register ;)

--- Versiones/Version 16-05-2025/Inicio_sesion.java
+++ Versiones/Version 16-05-2025/Inicio_sesion.java
@@ -12,18 +12,21 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
-public class Inicio_sesion extends JFrame {
+public class Inicio_sesion extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textFieldUsuario;
-	private JTextField textFieldContraseña;
 	private final String URL= "jdbc:mysql://localhost:33306/hundir_la_flota";
 	private final String USER="root";
 	private final String PASSWORD="alumnoalumno";
@@ -31,20 +34,51 @@ public class Inicio_sesion extends JFrame {
 	private static PreparedStatement stmt;
 	private String usuario;
 	private String contr;
+	private boolean sesionIniciada = false;
+	private JPasswordField passwordField;
+
+	
+	
+	
+	public boolean isSesionIniciada() {
+	    return sesionIniciada;
+	    
+	    
+	}
+	
+	public String getUsuario() {
+	    return usuario;
+	}
 
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				EventQueue.invokeLater(() -> {
 				try {
-					Inicio_sesion frame = new Inicio_sesion();
-					frame.setVisible(true);
+					
+					 // Ventana dummy para ser el padre
+			        JFrame dummy = new JFrame();
+			        dummy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			        Inicio_sesion login = new Inicio_sesion(dummy);
+			        login.setVisible(true); // Modal: bloquea aquí
+
+			        if (login.isSesionIniciada()) {
+			        	SwingUtilities.invokeLater(() -> {
+		                    Menu_principal menu = new Menu_principal(login.getUsuario());// puedes pasar el nombre si quieres
+		                    menu.setVisible(true);// JFrame: no bloquea nada
+		                });
+			  
+			        } else {
+			            //System.exit(0); // No inició sesión → cerrar app
+			        }
+					
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				});
 			}
 		});
 	}
@@ -52,18 +86,20 @@ public class Inicio_sesion extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Inicio_sesion() {
+	public Inicio_sesion(JFrame Menu_principal) {
+		
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection=DriverManager.getConnection(URL,USER,PASSWORD);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 		
 		setTitle("Iniciar sesion");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 855, 452);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -76,12 +112,6 @@ public class Inicio_sesion extends JFrame {
 		textFieldUsuario.setBounds(334, 163, 153, 19);
 		contentPane.add(textFieldUsuario);
 		textFieldUsuario.setColumns(10);
-		
-		
-		textFieldContraseña = new JTextField();
-		textFieldContraseña.setBounds(334, 215, 153, 19);
-		contentPane.add(textFieldContraseña);
-		textFieldContraseña.setColumns(10);
 		
 		
 		JLabel lblNewLabelUser = new JLabel("Usuario:");
@@ -97,9 +127,16 @@ public class Inicio_sesion extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 				Registrarse dialog = new Registrarse(Inicio_sesion.this); 
-				dialog.setVisible(true); 
+		        dialog.setVisible(true);
 			}
 		});
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(334, 214, 153, 21);
+		contentPane.add(passwordField);
+		
+		String contraseña = new String(passwordField.getPassword());
+		
 		btnNewButton.setBounds(263, 261, 117, 21);
 		contentPane.add(btnNewButton);
 
@@ -114,7 +151,6 @@ public class Inicio_sesion extends JFrame {
 				
 				
 				usuario = textFieldUsuario.getText();
-				contr = textFieldContraseña.getText();
 
 
 				try {
@@ -129,8 +165,12 @@ public class Inicio_sesion extends JFrame {
 					//Y SI COINCIDE SE PONE A TRUE SU BOOLEANO 
 					if (rs.next()) {
 						existe=true;
+					
+						String contrasenyaOriginal = new String(passwordField.getPassword());
+			            String contrasenyaCifrada = ContrasenyaCifrada.hashContrasenya(contrasenyaOriginal);
+
 						
-						if(contr.equals(rs.getString("password_usuario"))){
+						if(contrasenyaCifrada.equals(rs.getString("password_usuario"))){
 								coincidepasswd=true;
 								
 							}	
@@ -141,8 +181,10 @@ public class Inicio_sesion extends JFrame {
 					
 						JOptionPane.showMessageDialog(contentPane,"Error: Usuario no existente","Error",JOptionPane.ERROR_MESSAGE);
 					}else if(coincidepasswd){ //SI EXISTE EL USUARIO COMPRUEBA QUE COINCIDA LA PASSWORD Y INICIA SESIÓN
-						setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-						System.exit(0);
+						sesionIniciada = true;
+						Menu_principal menu = new Menu_principal(textFieldUsuario.getText());
+						menu.setVisible(true);
+						dispose();
 						
 					}else { // SI NO COICIDE LA PASSWORD SALTA MENSAJITO ERROR
 					
@@ -160,8 +202,11 @@ public class Inicio_sesion extends JFrame {
 		btnIniciarSesion.setBounds(408, 261, 117, 21);
 		contentPane.add(btnIniciarSesion);
 		
+		
+		
 		//codigo para menu principal (se abre auto al abrir otra)
-		//Registrarse dialog = new Registrarse(this);
+		
+		//Inicio_sesion dialog = new Inicio_sesion(dialog Menu_principal);
 		//dialog.setVisible(true);
 		
 	}
