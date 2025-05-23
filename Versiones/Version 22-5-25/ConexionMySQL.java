317b879a1a579832b68e7af5b8ec421cfbcc8287
RubenR16 <rubenroigsoucase@gmail.com>
2025-05-23 08:18:47 +0200

    Add files via upload

---
+++ Versiones/Version 22-5-25/ConexionMySQL.java
@@ -0,0 +1,36 @@
+package com.proyecto.hundir_la_flota;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+	public class ConexionMySQL {
+		
+	private static Connection con;
+	
+	private final String URL= "jdbc:mysql://localhost:33306/hundir_la_flota";
+	private final String USER="root";
+	private final String PASSWORD="alumnoalumno";
+	
+	public ConexionMySQL(){
+		
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					con=DriverManager.getConnection(URL,USER,PASSWORD);
+				} catch (ClassNotFoundException e) {
+
+					e.printStackTrace();
+				} catch (SQLException e) {
+
+					e.printStackTrace();
+				}
+				
+			
+}
+	
+	public Connection getCon() {
+		return con;
+	}
+	
+	
+}
